f9f473cd2d3955310174046fc1084c231553baf3
Juan Snow <victor.liptak@gmail.com>
2023-03-07 00:35:15 +0100

    such a huge changes on all fronts

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-
-
-
-
 //CONST, VAR, LET
 
 //card
+
 const cards = document.querySelector('.cards');
 const images = document.querySelectorAll('.card__img');
 const backgrounds = document.querySelectorAll('.card__bg');
@@ -13,99 +10,76 @@ const calcValue = (a, b) => ((a / b) * range - range / 2).toFixed(1);
 let timeout;
 
 //different devices
+const nav = document.querySelector('nav');
 const desktops = document.querySelectorAll('.desktop');
 const container = document.querySelectorAll('.container')
+
+//SLIDER
+function hide(element) {
+    element.style.setProperty('display', 'none', element.style.getPropertyPriority('display'));
+}
+
+function hideAll() {
+    for (var i = 0; i < desktops.length; i++) {
+        hide(desktops[i]);
+    }
+}
+
+function show(element) {
+    element.style.setProperty('display', 'block', element.style.getPropertyPriority('display'));
+}
+
+function cardTogller (x, y) {
+    document.getElementById(x).addEventListener('click', function () {
+        hideAll();
+        show(document.getElementById(y));
+    }, false);
+}
+
+cardTogller ('link-one', 'one');
+cardTogller ('link-two', 'two');
+cardTogller ('link-three', 'three');
+cardTogller ('link-four', 'four');
+cardTogller ('link-five', 'five');
+
+show(document.getElementById('one'));
+
+//CHANGE ELEMENT SCALE
+
+container.forEach((element) => {
+    element.classList.add('scaler');
+});
 
 //find out if device has mouse or touchscreen
 const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
 
-//MOUSE DEVICES
-if (! isTouchDevice) { 
-    //console.log('Zariadenie nemá dotykovú obrazovku');
-
-    //SLIDER
-    function hide(element) {
-        element.style.setProperty('left', '-100%', element.style.getPropertyPriority('left'));
-    }
-
-    function hideAll() {
-        for (var i = 0; i < desktops.length; i++) {
-            hide(desktops[i]);
-        }
-    }
-
-    function show(element) {
-        element.style.setProperty('left', '0', element.style.getPropertyPriority('left'));
-    }
-
-    function cardTogller (x, y) {
-        document.getElementById(x).addEventListener('click', function () {
-            hideAll();
-            show(document.getElementById(y));
-        }, false);
-    }
-
-    cardTogller ('link-one', 'one');
-    cardTogller ('link-two', 'two');
-    cardTogller ('link-three', 'three');
-    cardTogller ('link-four', 'four');
-    cardTogller ('link-five', 'five');
-
-    show(document.getElementById('one'));
-    
-    //CHANGE ELEMENT SCALE ON HOVER
-    
-    container.forEach((element) => {
-        element.classList.add('scaler');
-    });
-    
-    //CARD EFFECT FOR LAPTOP/MAC/PC
-    document.addEventListener('mousemove', ({ x, y }) => {
-        if (timeout) {
-            window.cancelAnimationFrame(timeout);
-        }
-
-        timeout = window.requestAnimationFrame(() => {
-        const yValue = calcValue(y, window.innerHeight);
-        const xValue = calcValue(x, window.innerWidth);
-
-        //CONTROL CARD ROTATION
-        cards.style.transform = `rotateX(${yValue}deg) rotateY(${-xValue}deg)`;
-
-        //CONTROLL IMAGE POSITION
-        [].forEach.call(images, (image) => {
-            image.style.transform = `translateX(${xValue * 0.75}px) translateY(${yValue * 0.75}px)`;
-        });
-
-        //CONTROLL BACKGROUND POSITION
-        [].forEach.call(backgrounds, (background) => {
-            background.style.backgroundPosition = `${xValue * 2}px ${yValue * 2}px`;
-        });
-        });
-    },
-    false
-    );
-}
-
 //TOUCHSCREENDEVICES
-else {
+if (isTouchDevice) { 
     //console.log('Zariadenie ma dotykovú obrazovku');
 
-    //GIVE SCROLL OPTION FOR TOUCHSCREENS
-    desktops.forEach((element) => {
-        element.style.position = 'static';
+    nav.classList.add('nav-mobile');
+
+    var computerButton = document.querySelectorAll('.nav-button-computer');
+    computerButton.forEach((element) => {
+        element.style.display = 'none';
     });
 
+    //GIVE SCROLL OPTION FOR TOUCHSCREENS
+    /*desktops.forEach((element) => {
+        element.style.position = 'static';
+        element.style.display = 'block';
+    });*/
+
     //HIDE NAVIGATION MENU
-    function hideNav() {
+    /*function hideNav() {
         var nav = document.querySelector('nav');
         nav.style.display = 'none';    
     }
-    hideNav();
+    hideNav();*/
    
 
     //CARD EFFECT FOR ANDROID/ *ios*
-    window.addEventListener('deviceorientation',({ alpha, beta, gamma }) => {
+    /*window.addEventListener('deviceorientation',({ alpha, beta, gamma }) => {
         if (timeout) {
         window.cancelAnimationFrame(timeout);
         }
@@ -129,5 +103,45 @@ else {
         });
     },
     false
-    );
+    );*/
+}
+
+//MOUSE DEVICES
+else {
+    //console.log('Zariadenie používa myš, nemá dotykovú obrazovku');
+
+    //NAVIGATION FOR MOUSE DEVICES
+    nav.classList.add('nav-computer');
+
+    var touchscreenButton = document.querySelectorAll('.nav-button-touchscreen');
+    touchscreenButton.forEach((element) => {
+        element.style.display = 'none';
+    });
+    
+    //CARD EFFECT FOR LAPTOP/MAC/PC
+    /*document.addEventListener('mousemove', ({ x, y }) => {
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
+
+        timeout = window.requestAnimationFrame(() => {
+        const yValue = calcValue(y, window.innerHeight);
+        const xValue = calcValue(x, window.innerWidth);
+
+        //CONTROL CARD ROTATION
+        cards.style.transform = `rotateX(${yValue}deg) rotateY(${-xValue}deg)`;
+
+        //CONTROLL IMAGE POSITION
+        [].forEach.call(images, (image) => {
+            image.style.transform = `translateX(${xValue * 0.35}px) translateY(${yValue * 0.35}px)`;
+        });
+
+        //CONTROLL BACKGROUND POSITION
+        [].forEach.call(backgrounds, (background) => {
+            background.style.backgroundPosition = `${xValue * 1}px ${yValue * 1}px`;
+        });
+        });
+    },
+    false
+    );*/
 }
